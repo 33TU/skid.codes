eaa53b9ffe638621086d008f7aefa40e9af102f8
33TU <eetu.programmer@gmail.com>
2022-12-03 18:29:35 +0200

    Add front end template.

--- frontend/src/routes/profile/index.tsx
+++ frontend/src/routes/profile/index.tsx
@@ -1,18 +1,22 @@
 import { h } from 'preact';
-import {useEffect, useState} from "preact/hooks";
+import { useEffect, useState } from 'preact/hooks';
 import style from './style.css';
 
-// Note: `user` comes from the URL, courtesy of our router
-const Profile = ({ user }) => {
-	const [time, setTime] = useState(Date.now());
-	const [count, setCount] = useState(10);
+interface Props {
+    user: string;
+}
 
-	useEffect(() => {
+// Note: `user` comes from the URL, courtesy of our router
+const Profile = ({ user }: Props) => {
+    const [time, setTime] = useState<number>(Date.now());
+    const [count, setCount] = useState<number>(0);
+
+    useEffect(() => {
 		let timer = setInterval(() => setTime(Date.now()), 1000);
 		return () => clearInterval(timer);
 	}, []);
 
-	return (
+    return (
 		<div class={style.profile}>
 			<h1>Profile: {user}</h1>
 			<p>This is the user profile for a user named { user }.</p>
@@ -26,6 +30,6 @@ const Profile = ({ user }) => {
 			</p>
 		</div>
 	);
-}
+};
 
 export default Profile;
